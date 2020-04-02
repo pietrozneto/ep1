86a785005bf38b93a58235f81e43649dfd3c4709
pietrozneto <62669885+pietrozneto@users.noreply.github.com>
2020-04-02 14:38:38 -0300

    Ciclo que repete o ciclo de apostas + mudancas de print

--- EPmudanças.py
+++ EPmudanças.py
@@ -8,13 +8,14 @@ def pass_line_bet(dado_mesa, Valor_PL):#função da aposta pass line
     if dado_mesa == 7 or dado_mesa == 11:
         lucro = Valor_PL
         print('Você ganhou {0} fichas'.format(Valor_PL))
-        print('Você tem {0} fichas'.format(fichas))
     elif dado_mesa == 2 or dado_mesa == 3 or dado_mesa == 12:
         lucro = -Valor_PL
         print('Você perdeu {0} fichas'.format(Valor_PL))
-        print('Você tem {0} fichas'.format(fichas))
     else:
         print('Etapa: Poit')
+        Valor_PL = Valor_P
+        Valor_PL = 0
+        point(dado_mesa, Valor_PL)
     return lucro
 def point(dado_mesa, Valor_P): #função da aposta point
     ciclo_point = True
@@ -24,12 +25,10 @@ def point(dado_mesa, Valor_P): #função da aposta point
         if point == dado_mesa:
             lucro = Valor_PL
             print('Você ganhou {0} fichas'.format(Valor_P))
-            print('Você tem {0} fichas'.format(fichas))
             ciclo_point = False
         elif dado_mesa == 7:
             lucro = -Valor_PL
             print('Você perdeu {0} fichas'.format(Valor_PL))
-            print('Você tem {0} fichas'.format(fichas))
             ciclo_point = False
     return lucro
 def field(dado_mesa, Valor_F): #função da aposta fields
@@ -65,22 +64,35 @@ PL = False
 F = False
 A = False
 T = False
+continuar_apostando = 'sim'
 print('Pass Line Bet, Field, Any Craps, Twelvez (PL/F/A/T)')
 x = input('Qual aposta tipo de aposta deseja fazer? ')
-while x!='nao':
-    if x == 'PL':
-        PL = True
-        Valor_PL = int(input('Quanto deseja apostar? '))
-    elif x == 'F':
-        F = True
-        Valor_F = int(input('Quanto deseja apostar? '))
-    elif x == 'A':
-        A = True
-        Valor_A = int(input('Quanto deseja apostar? '))
-    elif x == 'T':
-        T = True
-        Valor_T = int(input('Quanto deseja apostar? '))
-    else:
-        break
-    print('Pass Line Bet, Field, Any Craps, Twelvez (PL/F/A/T)')
-    x = input('Qual aposta tipo de aposta deseja fazer? ')
+while fichas != 0 or continuar_apostando == 'nao':
+    if PL == True:
+        lucro += pass_line_bet(dado_mesa, Valor_PL)
+    if F == True:
+        lucro += field(dado_mesa, Valor_F)
+    if A == True:
+        lucro += any_craps(dado_mesa, Valor_A)
+    if T == True:
+        lucro += twelve(dado_mesa, Valor_T)
+    
+    while x!='nao':
+        if x == 'PL':
+            PL = True
+            Valor_PL += int(input('Quanto deseja apostar? '))
+        elif x == 'F':
+            F = True
+            Valor_F += int(input('Quanto deseja apostar? '))
+        elif x == 'A':
+            A = True
+            Valor_A += int(input('Quanto deseja apostar? '))
+        elif x == 'T':
+            T = True
+            Valor_T += int(input('Quanto deseja apostar? '))
+        else:
+            break
+        print('Pass Line Bet, Field, Any Craps, Twelvez, Não (PL/F/A/T/nao)')
+        x = input('Deseja fazer outra aposta ? ')
+
+print('cabo')
