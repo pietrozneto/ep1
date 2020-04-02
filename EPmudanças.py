6d2041895c3e95945955b1a8113dcc0e58d5acba
pietrozneto <62669885+pietrozneto@users.noreply.github.com>
2020-04-02 18:48:53 -0300

    melhorias na fase point e intervalos entre as partes

--- EPmudanças.py
+++ EPmudanças.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from time import sleep
 def dado_mesa(fichas): #função que define o valor da soma dos dados
     dado_mesa = randint(1,6)+ randint(1,6)
     print('Os dados deram {0}'.format(dado_mesa))
@@ -13,25 +13,28 @@ def pass_line_bet(dado_mesa, Valor_PL):#função da aposta pass line
         print('Você perdeu {0} fichas'.format(Valor_PL))
     else:
         print('Etapa: Poit')
+        sleep(1)
         Valor_PL = Valor_P
         Valor_PL = 0
+        dado_point = dado_rodada
         point(dado_mesa, Valor_PL)
     return lucro
-def point(dado_mesa, Valor_P): #função da aposta point
+def point(dado_point, Valor_P): #função da aposta point
     ciclo_point = True
-    point = dado_mesa
     while ciclo_point:
-        dado_mesa = dado_mesa(fichas)
-        if point == dado_mesa:
+        dado_rodada = dado_mesa(fichas)
+        if dado_point == dado_rodada:
             lucro = Valor_PL
             print('Você ganhou {0} fichas'.format(Valor_P))
+            print('Você tem {0} fichas'.format(fichas))
             ciclo_point = False
         elif dado_mesa == 7:
             lucro = -Valor_PL
             print('Você perdeu {0} fichas'.format(Valor_PL))
+            print('Você tem {0} fichas'.format(fichas))
             ciclo_point = False
     return lucro
-def field(dado_mesa, Valor_F): #função da aposta fields
+def field(dado_rodada, Valor_F): #função da aposta fields
     if dado_mesa >=5 and dado_mesa <= 8:
         lucro = -Valor_F
     elif dado_mesa == 2:
@@ -41,13 +44,13 @@ def field(dado_mesa, Valor_F): #função da aposta fields
     else:
         lucro = Valor_F
     return lucro
-def any_craps(dado_mesa, Valor_A): #função da aposta any craps
+def any_craps(dado_rodada, Valor_A): #função da aposta any craps
     if dado_mesa == 2 or dado_mesa==3 or dado_mesa==12:
         lucro = 7*Valor_A
     else:
         lucro = -Valor_A
     return lucro
-def twelve(dado_mesa, Valor_T): #função da aposta twelve
+def twelve(dado_rodada, Valor_T): #função da aposta twelve
     if dado_mesa == 12:
         lucro = -12*Valor_T
     else:
@@ -68,28 +71,42 @@ continuar_apostando = 'sim'
 print('Pass Line Bet, Field, Any Craps, Twelvez (PL/F/A/T)')
 x = input('Qual aposta tipo de aposta deseja fazer? ')
 while fichas != 0 or continuar_apostando == 'nao':
+    if PL or F or A or T:
+        print('Você tem {} fichas'.format(fichas))
+        dado_rodada = dado_mesa(fichas)
     if PL == True:
-        lucro += pass_line_bet(dado_mesa, Valor_PL)
+        lucro += pass_line_bet(dado_rodada, Valor_PL)
     if F == True:
-        lucro += field(dado_mesa, Valor_F)
+        lucro += field(dado_rodada, Valor_F)
     if A == True:
-        lucro += any_craps(dado_mesa, Valor_A)
+        lucro += any_craps(dado_rodada, Valor_A)
     if T == True:
-        lucro += twelve(dado_mesa, Valor_T)
-    
+        lucro += twelve(dado_rodada, Valor_T)
+    PL = False
+    F = False
+    A = False
+    T = False
     while x!='nao':
         if x == 'PL':
             PL = True
+            print('Você tem {} fichas'.format(fichas))
             Valor_PL += int(input('Quanto deseja apostar? '))
+            fichas -= Valor_PL
         elif x == 'F':
             F = True
+            print('Você tem {} fichas'.format(fichas))
             Valor_F += int(input('Quanto deseja apostar? '))
+            fichas -= Valor_F
         elif x == 'A':
             A = True
+            print('Você tem {} fichas'.format(fichas))
             Valor_A += int(input('Quanto deseja apostar? '))
+            fichas -= Valor_A
         elif x == 'T':
             T = True
+            print('Você tem {} fichas'.format(fichas))
             Valor_T += int(input('Quanto deseja apostar? '))
+            fichas -= Valor_T
         else:
             break
         print('Pass Line Bet, Field, Any Craps, Twelvez, Não (PL/F/A/T/nao)')
